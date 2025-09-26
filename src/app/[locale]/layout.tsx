6969c92a64103b9eea4d5f98ba40e08b31ffff12
GaaferAlwakeil <venditathisme@gmail.com>
2025-09-26 15:26:14 +0300

    refactor: improve layout structure and update global styles

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }) {
    return (
       <html>
-         <body className={`${ibmPlexSansArabic.className} `}>{children}</body>
+         <body className={`${ibmPlexSansArabic.className} `}>
+            {children}
+         </body>
       </html>
    );
 }
